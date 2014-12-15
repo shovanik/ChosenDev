9b397c3f628d9be2442f58834f036fdcaca81468
Shovanik Majumdar <shovanik.majumdar@appsbee.com>
2014-12-15 19:16:34 +0530

    Initial Commit

---
+++ Chosen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chosen
+//
+//  Created by appsbee on 15/12/14.
+//  Copyright (c) 2014 appsbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
